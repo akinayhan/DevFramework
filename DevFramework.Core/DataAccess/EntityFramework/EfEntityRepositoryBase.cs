7231778d047d0fd091e7dbf687e85907689b15ce
akın ayhan <akinayhan@hotmail.com.tr>
2021-07-31 19:03:10 +0300

    entity

---
+++ DevFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContex> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContex : DbContext, new()
+    {
+        TEntity IEntityRepository<TEntity>.Add(TEntity entity)
+        {
+            using (var context = new TContex())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        void IEntityRepository<TEntity>.Delete(TEntity entity)
+        {
+            using (var context = new TContex())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+  
+            }
+        }
+
+        TEntity IEntityRepository<TEntity>.Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context = new TContex())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        List<TEntity> IEntityRepository<TEntity>.GetList(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context = new TContex())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        TEntity IEntityRepository<TEntity>.Update(TEntity entity)
+        {
+            using (var context = new TContex())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+    }
+}
